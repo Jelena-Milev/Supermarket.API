456f23ec1c5c51b04014ab98f42348419d50ac92
Jelena-Milev <jeka.milev@gmail.com>
2019-10-10 17:20:56 +0200

    Project and part of controllers created

---
+++ Services/CategoryService.cs
@@ -0,0 +1,17 @@
+﻿using Supermarket.API.Domain.Models;
+using Supermarket.API.Domain.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.API.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        public Task<IEnumerable<Category>> ListAsync()
+        {
+            return null;
+        }
+    }
+}
